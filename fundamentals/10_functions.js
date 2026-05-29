bf07eb75975720d2f35cf1c7c74b45aa27fd187e
Purus <purusottampaswan925@gmail.com>
2026-05-29 17:44:40 +0530

    function

--- fundamentals/10_functions.js
+++ fundamentals/10_functions.js
@@ -155,58 +155,58 @@ ______________Scopes____________________
 
 
 
-const user = {
-    username: "purus",
-    price: 999,
+// const user = {
+//     username: "purus",
+//     price: 999,
 
-    welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
-    }
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
 
-}
+// }
 
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+// // user.welcomeMessage()
+// // user.username = "sam"
+// // user.welcomeMessage()
 
-// console.log(this);
+// // console.log(this);
 
-// function chai(){
+// // function chai(){
+// //     let username = "hitesh"
+// //     console.log(this.username);
+// // }
+
+// // chai()
+
+// // const chai = function () {
+// //     let username = "hitesh"
+// //     console.log(this.username);
+// // }
+
+// const chai =  () => {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this);
 // }
 
-// chai()
 
-// const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+// // chai()
 
-const chai =  () => {
-    let username = "hitesh"
-    console.log(this);
-}
+// // const addTwo = (num1, num2) => {
+// //     return num1 + num2
+// // }
 
+// // const addTwo = (num1, num2) =>  num1 + num2
 
-// chai()
+// // const addTwo = (num1, num2) => ( num1 + num2 )
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
-
-// const addTwo = (num1, num2) =>  num1 + num2
-
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))
 
 
-// const myArray = [2, 5, 3, 7, 8]
+// // const myArray = [2, 5, 3, 7, 8]
 
-// myArray.forEach()
+// // myArray.forEach()
