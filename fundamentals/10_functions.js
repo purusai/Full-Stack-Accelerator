bd5c9d604ffd240b3ec5e71fdf2486b83b985b93
Purus <purusottampaswan925@gmail.com>
2026-05-29 17:41:16 +0530

    function

--- fundamentals/10_functions.js
+++ fundamentals/10_functions.js
@@ -143,3 +143,31 @@ ______________Scopes____________________
 // const addTwo = function(num){
 //     return num + 2
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+const user = {
+    username: "purus",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
+
+console.log(this);
