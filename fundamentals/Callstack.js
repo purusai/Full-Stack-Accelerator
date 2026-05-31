991b4f52361becb70e0f472b97176a2735d23fdc
Purus <purusottampaswan925@gmail.com>
2026-05-31 10:05:41 +0530

    callstack

--- fundamentals/Callstack.js
+++ fundamentals/Callstack.js
@@ -3,5 +3,21 @@ CallStack understanding
 
 LIFO: Last In First Out
 
+function one() {
+  console.log("one")
+  two()
+}
+
+function two(){
+  console.log("two")
+  three()
+  }
+
+ function three(){
+   console.log("three")
+   } 
+
+
+
 
 */
