38a1069f88adf3e7e7c155ece4e984cf1dbd30d2
Purus <purusottampaswan925@gmail.com>
2026-06-01 17:34:06 +0530

    map

--- 03_iterations/loop.js
+++ 03_iterations/loop.js
@@ -136,3 +136,40 @@
 // //     console.log(key, ':-', value);
     
 // // }
+
+
+
+
+
+
+
+
+
+
+
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+
+// for (const key in myObject) {
+//     //console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     //console.log(programming[key]);
+// }
+
+// // const map = new Map()
+// // map.set('IN', "India")
+// // map.set('USA', "United States of America")
+// // map.set('Fr', "France")
+// // map.set('IN', "India")
+
+// // for (const key in map) {
+// //     console.log(key);
+// // }
