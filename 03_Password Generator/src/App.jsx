a858ef90f896c0548e62d1757ae37785d2224643
Purus <purusottampaswan925@gmail.com>
2026-06-15 00:31:28 +0530

    Tailwind CSS

--- 03_Password Generator/src/App.jsx
+++ 03_Password Generator/src/App.jsx
@@ -1,120 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { useState, useCallback } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*()_{}~+"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+
+
+    
+    }
+    setPassword(pass)
+
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg p-4 my-8 text-gray-400 bg-zinc-900'>
+      <h1 className='text-pink-500 text-center mb-4'>Password Generator</h1>
+     <div className='flex  shadow rounded-lg overflow-hidden bg-zinc-950'>
+      <input
+     type="text"
+     value={password}
+     className="outline-none w-full py-1 px-3"
+     placeholder="password"
+     readOnly
+     />
+     <button className='outline-none text-gray-400 hover:text-white px-3 py-0.5 shrink-0'
+     >copy</button>
+     </div>
+     <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center py-3 gap-x-1'>
+        <input 
+          type="range" 
+          min={8}
+          max={100}
+          value={length}
+          className='cursor-pointer accent-pink-700'
+          onChange={(e) => {setLength(e.target.value)}}
+          
+        />
+        <label>Length: {length} </label>
+      </div>
+      <div className='flex items-center py-3 gap-x-1'>
+         <input 
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              className='cursor-pointer accent-pink-500'
+              onChange={(e) => {
+                   setNumberAllowed((prev) => !prev);
+              }}
+         
+         />
+         <label htmlFor='numberInput'>Numbers</label>
+      </div>
+        
+        <div></div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+     </div>
+    </div>
+      
     </>
   )
 }
