3c9a8a100072618cbd7b3850c0ea1e47b119b8e7
Purus <purusottampaswan925@gmail.com>
2026-06-17 03:11:39 +0530

    initiate new project

---
+++ 04_Currency_convertor/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-pink-500'>its Purus</h1>
+    </>
+  )
+}
+
+export default App
