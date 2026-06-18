52139cb435a11dbb89454800b40c2ef364ee9cf7
Purus <purusottampaswan925@gmail.com>
2026-06-18 17:28:16 +0530

    fix

--- 05_React_Router/src/components/Github/Github.jsx
+++ 05_React_Router/src/components/Github/Github.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect, useState } from 'react'
 
 const Github = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState(null)
+
     useEffect(() => {
-        const response = async (){
-          fetch("https://api.github.com/users/purusxyz")
-        .then(res => res.json)
-        .then(data => {
-            console.log(data);
-            setData(data)
-        })
+        const fetchData = async () => {
+            const res = await fetch("https://api.github.com/users/purusxyz")
+            const json = await res.json()
+            console.log(json)
+            setData(json)
         }
-        
-       
+
+        fetchData()
     }, [])
-  return (
-    <div className='text-center m-4 bg-gray-950 text-white p-4 text-3xl'>
-      Github followers: {data.followers}
-      <img 
-      className=''
-      src={data.avatar_url} alt="Git Profile picture" width={300} />
-    </div>
-  )
+
+    return (
+        <div className='text-center m-4 bg-gray-950 text-white p-4 text-3xl'>
+            
+            {data ? (
+                <>
+                    <p>Github followers: {data.followers}</p>
+                    <img 
+                        src={data.avatar_url} 
+                        alt="Git Profile" 
+                        width={300} 
+                        className="mx-auto mt-4 rounded-full"
+                    />
+                </>
+            ) : (
+                <p>Loading...</p>
+            )}
+
+        </div>
+    )
 }
 
 export default Github
