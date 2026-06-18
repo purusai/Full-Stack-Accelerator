4ae99acfa78d6fa75447e6333b99da4f3e0a2261
Purus <purusottampaswan925@gmail.com>
2026-06-18 13:50:45 +0530

    ui

--- 04_Currency_convertor/src/App.jsx
+++ 04_Currency_convertor/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
             }}
         >
             <div className="w-full">
-                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/2">
                     <form
                         onSubmit={(e) => {
                             e.preventDefault();
@@ -55,7 +55,7 @@ function App() {
                         <div className="relative w-full h-0.5">
                             <button
                                 type="button"
-                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-700 rounded-md bg-pink-600 hover:bg-pink-700 text-white px-2 py-0.5"
                                 onClick={swap}
                             >
                                swap
@@ -72,7 +72,7 @@ function App() {
                                 
                             />
                         </div>
-                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                        <button type="submit" className="w-full bg-pink-700 hover:bg-pink-800 text-white px-4 py-3 rounded-lg">
                             Convert {from.toUpperCase()} to {to.toUpperCase()}
                         </button>
                     </form>
--- 04_Currency_convertor/src/components/Input.jsx
+++ 04_Currency_convertor/src/components/Input.jsx
@@ -17,7 +17,7 @@ function Input({
    
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+        <div className={`bg-white/50 p-3 rounded-lg text-sm flex `}>
             <div className="w-1/2">
                 <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
@@ -37,7 +37,7 @@ function Input({
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
-                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    className="rounded-lg px-1 py-1 bg-gray-100/50 cursor-pointer outline-none"
                     value = {selectCurrency}
                     onChange={(e) => onCurrencyChange &&
                        onCurrencyChange(e.target.value)}
