da67a2f3479f45e3ae4f5a1e804c0a80c76d6a20
Purus <purusottampaswan925@gmail.com>
2026-06-20 04:03:09 +0530

    fix

--- 07_Theme_Switcher/src/components/Card.jsx
+++ 07_Theme_Switcher/src/components/Card.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function Card() {
     return (
-        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
-            </a>
+        <div className="w-full bg-white border border-gray-200 rounded-2xl shadow dark:bg-gray-950 dark:border-gray-700">
+            <Link to="/">
+                <img className="p-8 rounded-t-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV529P0V3Odt6XMvcgbbmDwtMXQVHnd9WBeWLvbTEspaAmgPZ_56GzOJI&s" alt="product_image1" />
+            </Link>
             <div className="px-5 pb-5">
-                <a href="/">
+                <Link to="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                         Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
                     </h5>
-                </a>
+                </Link>
                 <div className="flex items-center mt-2.5 mb-5">
                     <svg
                         className="w-4 h-4 text-yellow-300 mr-1"
@@ -63,13 +64,13 @@ export default function Card() {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a
-                        href="/"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$700</span>
+                    <Link
+                        to="/"
+                        className="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-zinc-700 dark:hover:bg-zinc-800 dark:focus:ring-zinc-800"
                     >
                         Add to cart
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
