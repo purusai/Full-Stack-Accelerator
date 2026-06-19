b030260582d53621d8fae28ce08d3f156fc5643a
Purus <purusottampaswan925@gmail.com>
2026-06-20 04:20:09 +0530

    fix

--- 07_Theme_Switcher/src/components/ThemeBtn.jsx
+++ 07_Theme_Switcher/src/components/ThemeBtn.jsx
@@ -23,7 +23,7 @@ export default function ThemeBtn() {
                 checked={themeMode === "dark"}
             
             />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-300 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-950"></div>
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-gray-300 dark:peer-focus:ring-blue-300 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-950"></div>
             <span className="ml-3 text-sm font-medium text-gray-900">Toggle Theme</span>
         </label>
     );
