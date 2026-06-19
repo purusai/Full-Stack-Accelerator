5c3df755420c5100865ad4471970c9f1a3bcf15d
Purus <purusottampaswan925@gmail.com>
2026-06-19 17:42:01 +0530

    add context

--- 07_Theme_Switcher/src/components/ThemeBtn.jsx
+++ 07_Theme_Switcher/src/components/ThemeBtn.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import useTheme from '../context/Theme';
 
 export default function ThemeBtn() {
     
+    const { themeMode, lightTheme, darkTheme} = useTheme()
+    const onChangeBtn = (e) => {
+        const darkModeStatus = e.currentTarget.checked
+        if (darkModeStatus) {
+            darkTheme()
+        } else {
+            lightTheme()
+        }
+    }
 
     return (
         <label className="relative inline-flex items-center cursor-pointer">
