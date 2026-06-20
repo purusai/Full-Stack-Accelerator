285fc4e8e1b7c5e0ca5c39631478dc7d2943270e
Purus <purusottampaswan925@gmail.com>
2026-06-20 23:34:31 +0530

    fix

--- 08_Todos_Manager/src/App.jsx
+++ 08_Todos_Manager/src/App.jsx
@@ -17,10 +17,10 @@ function App() {
     setTodos((prev) => prev.filter((todo) => todo.id !== id))
   }
 
-  const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === 
-    id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
-  }
+  // const toggleComplete = (id) => {
+  //   setTodos((prev) => prev.map((prevTodo) => prevTodo === 
+  //   id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  // }
 
 
   return (
